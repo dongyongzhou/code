044f04e7c4ba90ca569b5fa1afd38ce7d0208a4c
Dongyong Zhou <dongyongzhou@foxmail.com>
2012-04-13 17:16:37 +0800

    first commit. Add testmail perl script

---
+++ README.markdown
@@ -0,0 +1,13 @@
+Code
+==================
+
+This is the code source repository.
+
+--Dongyong Zhou
+
+The content of the site (document content, blog posts and page content) is licensed under a Creative Commons Attribution-Non Commercial-Share Alike 3.0 license (you may use it, but only under the same license and you must give attribution).
+
+Contributing
+============
+
+Fork, update, send a pull request.
---
+++ scripts/testmail.pl
@@ -0,0 +1,47 @@
+#!/usr/bin/perl
+
+use LWP::UserAgent;
+use LWP::Protocol::https;
+use strict;
+use warnings;
+
+# the perl script is used to test sendmail function of my server
+# It post a http request via https.
+# user authentication message is needed.
+
+# This is the sendMail URL, with API provided by server.
+my $url = 'https://qrd-dm.qualcomm.com/sendMail';
+
+# param
+my $from = '';
+my $to = '';
+my $subject = 'Send mail test';
+my $content = 'Hello mail';
+
+# Username
+my $username = '';
+
+# Makes a prompt for you to enter your password
+my $password;
+print "Enter password: ";
+system('stty','-echo');
+chop($password=<STDIN>);
+system('stty','echo');
+
+my $ua = LWP::UserAgent->new();
+$ua->protocols_allowed(['https']);
+
+my $response = $ua->post($url,
+                    Content_Type => 'form-data',
+                    Content => [from => $from,
+                    to => $to,
+                    subject => $subject,
+                    content => $content
+                    user_name => $username,
+		    password => $password
+                    ]);
+
+printf "GG %s\n", $response->status_line;
+my $resp_cont = $response->content();
+print "GG $resp_cont\n";
+
